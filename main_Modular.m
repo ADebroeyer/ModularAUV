56dcc94921fb8874257b077330fc272224bc5016
Arnaud Debroeyer <arnaud.dO2@gmail.com>
2024-03-18 19:03:33 +0100

    compSIM mooier maken

--- main_Modular.m
+++ main_Modular.m
@@ -1,4 +1,24 @@
 function [xdot,U] = main_Modular(x,ui,Vc,betaVc,w_c)
+% main function for which is calling all the modules
+% called in the SIMULINK model: ModularSIM.slx and run_main.m
+%
+% Dnu_c         derivative of current velocities
+% M             mass + hydrodynamic added mass
+% tau           propulsion
+% tau_liftdrag  hydrodynamic lift and drag forces of a submerged 
+%               "wing profile" for varying angle of attack
+%t au_crossflow  cross-flow drag integrals for
+%               a marine craft using strip theory.
+% C             mass + hydrodynamic added damping
+%               CA-terms in roll, pitch and yaw can destabilize the model if quadratic
+%               rotational damping is missing. These terms are assumed to be zero
+% nu_r          relative velocity
+% D             linear damping matrix for marine craft
+% J,R           rotation matrices
+% g             vector of restoring 
+%               forces about an arbitrarily point CO for a submerged body
+% xdot
+% nu            state vector: x(1:6)
 
 S = load("ModelParameters.mat");
 
@@ -57,26 +77,6 @@ tau(4) = M_prop(1);  % scaled down by a factor of 10 to match exp. results
 tau(5) = M_prop(2);
 tau(6) = M_prop(3);
 
-%% State-space model
-
-% Dnu_c         derivative of current velocities
-% M             mass + hydrodynamic added mass
-% tau           propulsion
-% tau_liftdrag  hydrodynamic lift and drag forces of a submerged 
-%               "wing profile" for varying angle of attack
-%tau_crossflow  cross-flow drag integrals for
-%               a marine craft using strip theory.
-% C             mass + hydrodynamic added damping
-%               CA-terms in roll, pitch and yaw can destabilize the model if quadratic
-%               rotational damping is missing. These terms are assumed to be zero
-% nu_r          relative velocity
-% D             linear damping matrix for marine craft
-% J,R           rotation matrices
-% g             vector of restoring 
-%               forces about an arbitrarily point CO for a submerged body
-% % % % g = gRvect(W,B,R,r_bg,r_bb); % commented for now and removed from
-% xdot
-% nu            state vector: x(1:6)
 
 xdot = [ Dnu_c + M \ ...
             (tau + tau_liftdrag + tau_crossflow - C * nu_r - D * nu_r - g)
